650de29546dc506bd1462414bfe924d32ab4c51c
Paco Ho <pacotimekeeper>
2024-07-30 22:15:03 +0800

    julia 1.11 upgrade

--- main.jl
+++ main.jl
@@ -1,2 +1,10 @@
+using Revise
 using WebTools
-
+using HTTP
+using Gumbo, Cascadia
+import Cascadia.matchFirst
+url = "https://www.dsat.gov.mo/hzmb/car_cross_2024.aspx"
+matchFirst(sel"td", url)
+resp = HTTP.get(url)
+doc = parsehtml(String(resp.body))
+matchFirst(sel"td", doc.root)
