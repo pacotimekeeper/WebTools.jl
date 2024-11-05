c1d554b38270707ea4c1303cf64863ef76b08224
Paco Ho <kaiipho@gmail.com>
2024-11-05 10:48:30 +0800

    add contentype responsecontent funcs

--- src/WebTools.jl
+++ src/WebTools.jl
@@ -6,6 +6,7 @@ using HTTP, Cascadia, Gumbo
 using StringEncodings
 
 export charset, responseContent, contentType
+export contenttype, responsecontent
 # html_tables
 # export read_html
 
@@ -25,6 +26,14 @@ function contentType(url::AbstractString) :: Union{AbstractString, Nothing}
     m !== nothing ? m.captures[1] |> String : nothing
 end
 
+function contenttype(url::AbstractString) :: Union{AbstractString, Nothing}
+    response = HTTP.get(url)
+    content_body = String(response.body)
+    pattern = r"charset=(.*?)\""
+    m = match(pattern, content_body)
+    m !== nothing ? m.captures[1] |> String : nothing
+end
+
 ##
 ##read_html is an wrapper function for html_tables with default selector and start_row giving dataframes from the url provided
 
@@ -36,6 +45,16 @@ Renaming the function into CamelCase
 
 =#
 function responseContent(response::HTTP.Messages.Response, encoding::AbstractString = "UTF-8")::HTMLElement
+    content = try
+        parsehtml(decode(deepcopy(response.body), encoding)).root
+    catch ## using UTF-8 decoding
+        println("Unable to decode with $(encoding), using UTF-8 instead")
+        parsehtml(String(deepcopy(response.body))).root
+    end
+    content
+end
+
+function responsecontent(response::HTTP.Messages.Response, encoding::AbstractString = "UTF-8")::HTMLElement
     content = try
         parsehtml(decode(deepcopy(response.body), encoding)).root
     catch ## using UTF-8 decoding
